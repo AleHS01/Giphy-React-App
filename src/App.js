e0c5a12a3b4f96089e13d0fd93443e9081113a26
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-06-17 23:08:03 -0400

    gif renders perfectly on the UI

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ function App() {
       const list = await axios.get(trendingURl);
 
       setTrandingGifsListState(list.data.data);
+
+      setRandomGifState({});
+      setSearchGifsListState([]);
     } catch (error) {
       console.error(error);
     }
@@ -29,6 +32,9 @@ function App() {
       const gif = await axios.get(randomURL);
 
       setRandomGifState(gif.data.data);
+
+      setSearchGifsListState([]);
+      setTrandingGifsListState([]);
     } catch (error) {
       console.error(error);
     }
@@ -37,57 +43,80 @@ function App() {
   const searchGif = async (termToSearch) => {
     const formattedTermToSearch = encodeURIComponent(termToSearch);
 
-    // const searchUrl = `http://api.giphy.com/v1/gifs/search?q=${formattedTermToSearch}&limit=52&api_key=${giphyApiKey}`;
     const searchUrl = `http://api.giphy.com/v1/gifs/search?q=${formattedTermToSearch}&api_key=${giphyApiKey}`;
 
     try {
       const list = await axios.get(searchUrl);
+
       setSearchGifsListState(list.data.data);
+      setTrandingGifsListState([]);
+      setRandomGifState({});
     } catch (error) {
       console.log(error);
     }
   };
 
+  useEffect(() => {
+    trendingGiphy();
+  }, []);
+
   return (
     <div className="App">
       <button onClick={trendingGiphy}>Trending</button>
-      <button onClick={searchGif}>Search</button>
+      {/* <button onClick={searchGif}>Search</button> */}
       <button onClick={randomGiphy}>Random</button>
-      {/* <div className="gifs-containter">
-        {trandingGifsList.map((trandingGif) => {
-          return (
-            <GifCard
-              gifUrl={trandingGif.url}
-              title={trandingGif.title}
-              videoSrc={trandingGif.images.preview.mp4}
-              //imageSrc={trandingGif.images.preview_gif.url}
-            />
-          );
-        })}
-      </div> */}
-      {/* <SearchField searchGif={searchGif} />
-      <div className="gifs-containter">
-        {searchGifsList.map((searchGifs) => {
-          return (
-            <GifCard
-              imageSrc={searchGifs.images["480w_still"].url}
-              gifUrl={searchGifs.url}
-              title={searchGifs.title}
-              videoSrc={searchGifs.images.preview.mp4}
-              //imageSrc={trandingGif.images.preview_gif.url}
-            />
-          );
-        })}
-      </div> */}
-      {/* <div className="gifs-containter">
-        <GifCard
-          imageSrc={randomGif.images["480w_still"].url}
-          gifUrl={randomGif.url}
-          title={randomGif.title}
-          videoSrc={randomGif.images.preview.mp4}
-          //imageSrc={trandingGif.images.preview_gif.url}
-        />
-      </div> */}
+      <SearchField searchGif={searchGif} />
+      {/* {renderGifInUI()} */}
+
+      {/* Rendering a list of  trending Gif if we get null or undefined nothings happends 
+      else if we get an object or anything else we can render the component with the data from the api and the list*/}
+
+      {trandingGifsList.length > 0 && (
+        <div className="gifs-containter">
+          {trandingGifsList.map((trandingGif) => {
+            return (
+              <GifCard
+                gifUrl={trandingGif.url}
+                title={trandingGif.title}
+                videoSrc={trandingGif.images.preview.mp4}
+                imageSrc={trandingGif.images["480w_still"].url}
+              />
+            );
+          })}
+        </div>
+      )}
+
+      {/* Rendering a list of  search Gif if we get null or undefined nothings happends 
+      else if we get an object or anything else we can render the component with the data from the api and the list*/}
+      {searchGifsList.length > 0 && (
+        <div className="gifs-containter">
+          {searchGifsList.map((searchGifs) => {
+            return (
+              <GifCard
+                imageSrc={searchGifs.images["480w_still"].url}
+                gifUrl={searchGifs.url}
+                title={searchGifs.title}
+                videoSrc={searchGifs.images.preview.mp4}
+                //imageSrc={trandingGif.images.preview_gif.url}
+              />
+            );
+          })}
+        </div>
+      )}
+
+      {/* Rendering single Random Gif if we get null or undefined nothings happends 
+      else if we get a string or anything else we can render the component with the data from the api */}
+      {randomGif.url && (
+        <div className="gifs-containter">
+          <GifCard
+            imageSrc={randomGif.images["480w_still"].url}
+            gifUrl={randomGif.url}
+            title={randomGif.title}
+            videoSrc={randomGif.images.preview.mp4}
+            //imageSrc={trandingGif.images.preview_gif.url}
+          />
+        </div>
+      )}
     </div>
   );
 }
