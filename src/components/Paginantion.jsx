2d50f7b9f2f02a7463fea73073af6c89da7e7b16
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-06-19 15:03:28 -0400

    style first part of the ui done, missing buttons, and header

--- src/components/Paginantion.jsx
+++ src/components/Paginantion.jsx
@@ -6,11 +6,15 @@ const Paginantion = (props) => {
   }
 
   return (
-    <nav>
-      <ul>
+    <nav className="page-navigation-nav">
+      <ul className="page-navigation-ul">
         {pageNumbers.map((number) => {
+          let classes = "page-button-item ";
+          if (number === props.currentPage) {
+            classes += "active";
+          }
           return (
-            <li key={number}>
+            <li key={number} className={classes}>
               <a onClick={() => props.pageSelected(number)} href="#">
                 {number}
               </a>
